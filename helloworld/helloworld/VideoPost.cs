69f77f4488395f2ae8122c1f6eb87d6e6412086c
Alex A. Kwashie <alex.kwashie@hotmail.com>
2019-04-29 20:30:15 +0100

    adding image & video like title & url

---
+++ helloworld/helloworld/VideoPost.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace helloworld
+{
+
+    class VideoPost: Post
+    {
+       protected string VideoURL { get; set; }
+        protected int Length { get; set; }
+
+        public VideoPost() { }
+
+        public VideoPost(string title, string sendByUsername, string videoURL, bool isPublic, int length)
+        {
+            this.ID = GetNextID();
+            this.title = title;
+            this.SendUsername = sendByUsername;
+            this.isPublic = isPublic;
+
+
+            //Videourl is member of Videopost but not of Post/Imagepost
+            this.VideoURL = videoURL;
+            this.Length = length;
+        }
+
+        public override string ToString()
+        {
+            //return base.ToString()   //default
+            return String.Format("{0} - {1} - {2} by {3}", this.ID, this.title, this.VideoURL, this.SendUsername);
+        }
+
+    }
+}
