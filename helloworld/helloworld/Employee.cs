7d6e02fad4ae0dc6f3592a60c6ecbfddbe80f97b
Alex A. Kwashie <alex.kwashie@hotmail.com>
2019-05-01 16:03:38 +0100

    working on Exercise(Oop)

---
+++ helloworld/helloworld/Employee.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace helloworld
+{
+    class Employee
+    {
+
+        protected string FirstName { get; set; }
+        protected string LastName { get; set; }
+        protected int Salary { get; set; }
+
+        public Employee()
+        {
+            FirstName = "John";
+            LastName = "Sherren";
+            Salary = 32555;
+        }
+        //Constructor
+        public Employee( string firstName , string lastName, int salary)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Salary = salary;
+        }
+
+
+        public void work()
+        {
+            Console.WriteLine("I am working");
+        }
+
+        public void pause()
+        {
+            Console.WriteLine("Pausing is Life");
+        }
+
+
+        public override string ToString()
+        {
+            //return base.ToString()   //default
+            return String.Format("My name is {0} {1} and I am paid {2}", this.FirstName, this.LastName, this.Salary);
+        }
+    }
+}
