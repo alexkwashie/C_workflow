df25d3907e3a71aceb7644fbfb73024678bba107
Alex A. Kwashie <alex.kwashie@hotmail.com>
2019-05-15 16:00:47 +0100

    Enams

---
+++ Lamda/Enamss/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Enamss
+{
+
+
+    enum Day { Mo, Tu, We, Th, Fr, Sa, Su}
+
+    enum Month { Jan, Feb, Mar, Apr, May, Jun, Juy, Aug, Sept, Nov, Dec}
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Day fr = Day.Fr;
+
+
+            Day a = Day.Fr;
+            Month jan = Month.Jan;
+
+
+            Console.WriteLine(fr == a);
+            Console.WriteLine((int)Day.Th);
+            Console.WriteLine(jan);
+
+
+
+
+
+            Console.Read();
+        }
+    }
+}
