2e7aae5dcaa4855df52ac3133adfaf8afa8769e6
alexkwashie <alex.kwashie@hotmail.com>
2019-05-16 22:56:38 +0100

    Started on Regular Expressions

---
+++ Lamda/RegexT/RegexT/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RegexT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
