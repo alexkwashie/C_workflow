c559733dc32a66ff8a50d2a8c620005c9fbfed18
alexkwashie <alex.kwashie@hotmail.com>
2019-05-16 22:42:09 +0100

    Done with Randoms

---
+++ Lamda/MathClass/RandomT/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace RandomT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Random Dice = new Random();
+
+            int dicePlates;
+
+
+            for (int i = 0; i <10; i++)
+            {
+                //Next takes 2 numbers
+                dicePlates = Dice.Next(1,8);
+                //Console.WriteLine(dicePlates);
+
+            }
+
+
+            Random guess = new Random();
+
+            int ans;
+            bool fol = true;
+            while (fol)
+            {
+                Console.WriteLine("Ask me any Questions");
+                Console.ReadLine();
+
+                ans = guess.Next(1, 4);
+
+                if (ans == 1)
+                {
+                    Console.WriteLine("Yes");
+                }
+                else if (ans == 2)
+                {
+                    Console.WriteLine("NO");
+                }
+                else
+                {
+                    Console.WriteLine("MayBe");
+                    fol = false;
+                    Console.WriteLine("Thank you, Restart program");
+
+                }
+            }
+
+            
+                
+            
+
+            Console.Read();
+        }
+    }
+}
