cb32d4e66af50ea74aa2d3ced7a67c755cdb426e
Alex A. Kwashie <alex.kwashie@hotmail.com>
2019-05-16 21:45:58 +0100

    Random Objects

---
+++ Lamda/MathClass/RandomT/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace RandomT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Random Dice = new Random();
+
+            int dicePlates;
+
+
+            for (int i = 0; i <10; i++)
+            {
+                //Next takes 2 numbers
+                dicePlates = Dice.Next(1,8);
+                Console.WriteLine(dicePlates);
+
+            }
+           
+        }
+    }
+}
