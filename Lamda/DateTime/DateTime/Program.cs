e379b7a91fb028cf5a07c99f3e2237816b24b274
alexkwashie <alex.kwashie@hotmail.com>
2019-05-17 22:26:19 +0100

    Sarting on DateTime etc

---
+++ Lamda/DateTime/DateTime/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DateTimeT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dateTime = new DateTime();
+
+            Console.WriteLine("My Bday is {0}", dateTime);
+
+            Console.WriteLine(DateTime.Today.DayOfYear);
+            Console.Read();
+        }
+    }
+}
