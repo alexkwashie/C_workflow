41faa91542ba0224a5a4b5ef16630cf9d44e7013
alexkwashie <alex.kwashie@hotmail.com>
2019-05-17 20:34:58 +0100

    Done with RegEx

--- Lamda/RegexT/RegexT/Program.cs
+++ Lamda/RegexT/RegexT/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace RegexT
 {
@@ -7,6 +8,25 @@ namespace RegexT
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            string pattern = @"\D"; //looking for digits
+
+            Regex regex = new Regex(pattern);
+
+            string text = "Hi there, My ID is 12928542";
+
+            MatchCollection MtchCollect = regex.Matches(text);
+
+            Console.WriteLine("{0} is my Code \n {1}", MtchCollect.Count, text ); //MtchCollect.Count- counts the number of numbers found
+
+            foreach (Match hit in MtchCollect) //Match identifies each number found in the set
+            {
+                GroupCollection group = hit.Groups; //GroupCollection groups the number as one list
+
+                Console.WriteLine("{0} found at: {1}", group[0].Value, group[0].Index);
+            }
+
+            Console.Read();
         }
     }
 }
